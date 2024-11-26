f634b1aef2d9e43481f6bda22a59fd15f99ae435
Layeeq <layqahmed@gmail.com>
2024-11-26 00:15:59 -0600

    Add reading topology in controller

--- src/controller.py
+++ src/controller.py
@@ -2,25 +2,38 @@
 
 import sys
 
-LOGFILE_STR = '../log/controller.log'
-INIT_ERROR_STR = 'Incorrect argument length. Expected: `./controller.py duration`. Duration must be an integer.'
+LOGFILE_STR = "../log/controller.log"
+TOPOLOGY_FILE_STR = "../topology"
+INIT_ERROR_STR = "Incorrect argument length. Expected: `./controller.py duration`. Duration must be an integer."
+
 
 class Controller:
     def __init__(self):
+        self.edges = set()
         self.logfile = open(LOGFILE_STR, "wt")
         self.write_log("*****STARTING CONTROLLER*****")
 
         if len(sys.argv) != 2:
             self.write_log(INIT_ERROR_STR)
             exit(1)
-        
+
         try:
             self.duration = int(sys.argv[1])
         except:
             self.write_log(INIT_ERROR_STR)
             exit(1)
-        
+
         self.write_log(f"Duration: {self.duration}")
+
+        # assumes topology file to be in the expected format of unidirectional edges
+        with open(TOPOLOGY_FILE_STR, "rt") as f:
+            all_lines = f.readlines()
+            for line in all_lines:
+                if line.strip() == "":
+                    continue
+                self.edges.add(tuple(line.split()))
+
+        self.write_log("Edges: " + str(self.edges))
 
     def write_log(self, value):
         if type(value) != str:
@@ -35,5 +48,6 @@ class Controller:
         self.write_log("****END****")
         self.logfile.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     Controller().execute()
